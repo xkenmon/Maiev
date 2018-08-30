dca053011d5aaa35b919cf389b8fef8b5879b655
bigmeng <xkenmon@gmail.com>
2018-08-30 19:23:00 +0800

    init

---
+++ monitor/src/main/java/com/xkenmon/maiev/monitor/MonitorApplication.java
@@ -0,0 +1,15 @@
+package com.xkenmon.maiev.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableAdminServer
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
